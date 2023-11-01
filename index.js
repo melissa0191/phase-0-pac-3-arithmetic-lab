76ace9fcb1f038fd71ec070b981c51d886c7db3d
melissavelasquezgreene <melissa0191@outlook.es>
2023-11-01 15:54:31 -0400

    updates index.js

---
+++ index.js
@@ -0,0 +1,71 @@
+function add() {
+    // we'll fill this in shortly
+  }
+
+  function subtract() {
+    // we'll fill this in shortly
+  }
+
+  function multiply() {
+    // we'll fill this in shortly
+  }
+
+  function divide() {
+    // we'll fill this in shortly
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  console.log(add(500, 27));
+
+
+
+function subtract(a, b) {
+  return a - b;
+}
+console.log(subtract(527, 2));
+
+
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply(67, 123));
+
+
+
+function divide(a, b) {
+  return a / b;
+}
+console.log(divide(15, 13.313725));
+
+
+function increment(n) {
+    // we'll fill this in shortly
+  }
+
+
+ function decrement(n) {
+    // we'll fill this in shortly
+  }
+ 
+  function increment(n) {
+    return n + 1;
+  }
+  console.log(increment(973));
+
+  function decrement(n) {
+  return n - 1;
+}
+
+console.log(decrement(710));
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
+
+  
